b443f3c324207bd21472030dd8a346ff31b71edf
Natanna <natannacortez@gmail.com>
2022-02-06 03:06:43 -0300

    feat show balance

--- src/controllers/transactionsController.js
+++ src/controllers/transactionsController.js
@@ -1,27 +1,29 @@
-import joi from 'joi';
 import db from '../db.js';
+import joi from 'joi';
 import { ObjectId } from 'mongodb';
 
-/*const valueSchema = joi.object({
-    value:joi.string().required(),
-    description:joi.string().required(),
-    type:joi.string().valid("entry","outflow").required(),
-})*/
+const valueSchema = joi.object({
+    value: joi.string().pattern(/[0-9][","]/).required(),
+    description: joi.string().required(),
+    type: joi.string().valid("entry", "outflow").required(),
+    userId: joi.string().required(),
+    date: joi.required()
+});
 export async function transaction(req, res) {
-   const autenticação=req.body;
-   console.log(autenticação);
+    const values = req.body;
+    console.log(values);
 
-    /*const validation = valueSchema.validate(req.body, { abortEarly: false });
+    const validation = valueSchema.validate(values, { abortEarly: false });
     if (validation.error) {
-        res.status(422).send(validation.error.details.map(error => error.message))
+        res.sendStatus(422).send(validation.error.details.map(error => error.message));
         return;
-    }*/
-   
-    try {       
+    }
+
+    try {
         const user = await db.collection('users').findOne({ _id: new ObjectId(req.body.userId) });
         console.log(user._id)
-        if(user){
-            await db.collection("transaction").insertOne({...req.body, idUser:user._id});
+        if (user) {
+            await db.collection("transaction").insertOne({ ...req.body, idUser: user._id });
             res.sendStatus(201)
             return;
         }
@@ -36,21 +38,20 @@ export async function getTransaction(req, res) {
     const token = authorization?.replace('Bearer ', '');
 
     const session = await db.collection("sessions").findOne({ token });
-        if (!session) {
-            return res.sendStatus(401);
-        }
-        
+    if (!session) {
+        return res.sendStatus(401);
+    }
 
-    try {        
 
-        const entries = await db.collection("transaction").find({idUser: session.idUser}).toArray();
-        console.log(entries);
-        if(!entries){
+    try {
+
+        const entries = await db.collection("transaction").find({ idUser: session.idUser }).toArray();
+
+        if (!entries) {
             res.sendStatus(404);
-            return 
+            return
         }
-
-        res.send(entries)
+      res.send(entries);
     } catch (error) {
         console.log(error);
         res.sendStatus(500);
