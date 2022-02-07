4f4188f36ecc6b7fa58d4df618ef39d3d24279b2
Natanna <natannacortez@gmail.com>
2022-02-07 16:45:19 -0300

    style: formatting limit regex

--- src/schemas/transactionSchema.js
+++ src/schemas/transactionSchema.js
@@ -1,7 +1,7 @@
 import joi from 'joi';
 
 const transactionSchema = joi.object({
-    value: joi.string().pattern(/^[0-9]{1,10}\,[0-9]{1,4}$/).required(),
+    value: joi.string().pattern(/^[0-9]{1,100}\,[0-9]{1,4}$/).required(),
     description: joi.string().required(),
     type: joi.string().valid("entry", "outflow").required(),
     userId: joi.string().required(),
